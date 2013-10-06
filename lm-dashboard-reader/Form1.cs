336b7f794b908ea104649abec4bbd9e5e175fac2
Louis Chatriot <louis.chatriot@gmail.com>
2013-10-06 17:45:47 +0200

    Example references are ok

--- lm-dashboard-reader/Form1.cs
+++ lm-dashboard-reader/Form1.cs
@@ -7,11 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections.ObjectModel;
+using LibUsbDotNet;
+using LibUsbDotNet.Info;
+using LibUsbDotNet.Main;
 
 namespace lm_dashboard_reader
 {
     public partial class Form1 : Form
     {
+        public static UsbDevice MyUsbDevice;
+
         public Form1()
         {
             InitializeComponent();
@@ -24,6 +30,44 @@ namespace lm_dashboard_reader
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Console.WriteLine("Bloupibloup");
+
+            // Dump all devices and descriptor information to console output.
+            UsbRegDeviceList allDevices = UsbDevice.AllDevices;
+
+            Console.WriteLine(allDevices.Count());
+
+            foreach (UsbRegistry usbRegistry in allDevices)
+            {
+                if (usbRegistry.Open(out MyUsbDevice))
+                {
+                    Console.WriteLine(MyUsbDevice.Info.ToString());
+                    for (int iConfig = 0; iConfig < MyUsbDevice.Configs.Count; iConfig++)
+                    {
+                        UsbConfigInfo configInfo = MyUsbDevice.Configs[iConfig];
+                        Console.WriteLine(configInfo.ToString());
+
+                        ReadOnlyCollection<UsbInterfaceInfo> interfaceList = configInfo.InterfaceInfoList;
+                        for (int iInterface = 0; iInterface < interfaceList.Count; iInterface++)
+                        {
+                            UsbInterfaceInfo interfaceInfo = interfaceList[iInterface];
+                            Console.WriteLine(interfaceInfo.ToString());
+
+                            ReadOnlyCollection<UsbEndpointInfo> endpointList = interfaceInfo.EndpointInfoList;
+                            for (int iEndpoint = 0; iEndpoint < endpointList.Count; iEndpoint++)
+                            {
+                                Console.WriteLine(endpointList[iEndpoint].ToString());
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            // Free usb resources.
+            // This is necessary for libusb-1.0 and Linux compatibility.
+            UsbDevice.Exit();
+
             label1.Text = "Salut";
         }
     }
